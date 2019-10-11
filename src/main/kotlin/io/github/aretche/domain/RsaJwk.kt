2b2e5a0740220ca33c9a426892ad43c004756ee8
arellanog <arellanog@cent.gov.ar>
2019-10-11 18:02:53 -0300

    Add RsaJwk persistent class

---
+++ src/main/kotlin/io/github/aretche/domain/RsaJwk.kt
@@ -0,0 +1,27 @@
+package io.github.aretche.domain
+
+import java.util.*
+import javax.persistence.*
+
+@Entity
+@Table(name = "rsa_jwk")
+class RsaJwk(
+        @Id
+        @GeneratedValue
+        val id: Long,
+
+        @Column(name = "kid", nullable = false)
+        val kid: String,
+
+        @Column(name = "private_jwk", length = 5000, nullable = false)
+        val privateJwk: String,
+
+        @Column(name = "public_jwk", length = 1000, nullable = false)
+        val publicJwk: String,
+
+        @Column(name = "active", nullable = false)
+        val active: Boolean,
+
+        @Column(name = "date_created", nullable = false)
+        val dateCreated: Date
+)
