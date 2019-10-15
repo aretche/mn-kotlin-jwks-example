7884cc94a961440bed458cd6fa4e3854d207a426
arellanog <arellanog@cent.gov.ar>
2019-10-15 02:37:11 -0300

    Add configuration for signing with persistent RSAKeys

---
+++ src/main/kotlin/io/github/aretche/PersistentRsaSignatureConfiguration.kt
@@ -0,0 +1,41 @@
+package io.github.aretche
+
+import com.nimbusds.jose.JWSAlgorithm
+import com.nimbusds.jose.jwk.RSAKey
+import io.micronaut.security.token.jwt.signature.rsa.RSASignatureGeneratorConfiguration
+import java.security.interfaces.RSAPrivateKey
+import java.security.interfaces.RSAPublicKey
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Singleton
+@Named("generator")
+class PersistentRsaSignatureConfiguration(private val rsaJwkRepository: RsaJwkRepositoryImpl): RSASignatureGeneratorConfiguration {
+
+   private var keyPair: RSAKey? = null
+
+    private fun ensureKeyPair(){
+        if (keyPair == null) {
+            val rsaJwk = rsaJwkRepository.getLast()
+            keyPair = if(rsaJwk == null){
+                RSAKey.parse(rsaJwkRepository.createKey()?.privateJwk)
+            } else {
+                RSAKey.parse(rsaJwk.privateJwk)
+            }
+        }
+    }
+
+    override fun getPublicKey(): RSAPublicKey {
+        ensureKeyPair()
+        return keyPair!!.toRSAPublicKey()
+    }
+
+    override fun getPrivateKey(): RSAPrivateKey {
+        ensureKeyPair()
+        return keyPair!!.toRSAPrivateKey()
+    }
+
+    override fun getJwsAlgorithm(): JWSAlgorithm {
+        return JWSAlgorithm.RS256
+    }
+}
