12684836f2f4ef74a4f44708c4ea35689a41045d
arellanog <arellanog@cent.gov.ar>
2019-10-15 02:46:11 -0300

    Remove JwkController

--- src/main/kotlin/io/github/aretche/JwkCommands.kt
+++
@@ -1,9 +0,0 @@
-package io.github.aretche
-
-import com.fasterxml.jackson.annotation.JsonInclude
-import io.github.aretche.domain.RsaJwk
-
-@JsonInclude(JsonInclude.Include.ALWAYS)
-data class Jwks(
-        val keys: List<RsaJwk>
-)
