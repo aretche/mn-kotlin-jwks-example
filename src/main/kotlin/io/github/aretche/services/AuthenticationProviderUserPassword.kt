616fa4227b1d0881991afceb241867a785f87aab
arellanog <arellanog@cent.gov.ar>
2019-10-15 02:38:32 -0300

    Add controller an Authentication provider to test JWT authentication

---
+++ src/main/kotlin/io/github/aretche/services/AuthenticationProviderUserPassword.kt
@@ -0,0 +1,24 @@
+package io.github.aretche.services
+
+import io.micronaut.security.authentication.AuthenticationProvider
+import io.micronaut.security.authentication.AuthenticationRequest
+import io.micronaut.security.authentication.AuthenticationResponse
+import org.reactivestreams.Publisher
+import io.micronaut.security.authentication.AuthenticationFailed
+import io.reactivex.Flowable
+import java.util.ArrayList
+import io.micronaut.security.authentication.UserDetails
+import javax.inject.Singleton
+
+@Singleton
+class AuthenticationProviderUserPassword : AuthenticationProvider {
+    override fun authenticate(authenticationRequest: AuthenticationRequest<*, *>?): Publisher<AuthenticationResponse> {
+        if (authenticationRequest!=null && authenticationRequest.identity != null && authenticationRequest.secret != null) {
+            if ( authenticationRequest.identity == "sherlock" && authenticationRequest.secret == "password" ) {
+                return Flowable.just<AuthenticationResponse>(UserDetails(authenticationRequest.identity as String, ArrayList()))
+            }
+        }
+        return Flowable.just<AuthenticationResponse>(AuthenticationFailed())
+    }
+}
+
