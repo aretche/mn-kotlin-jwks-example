ce704eb67e6e066137746f8a75f4f67d935d149f
arellanog <arellanog@cent.gov.ar>
2019-10-16 17:31:07 -0300

    Add endpoint with custom signed JWT

--- src/main/kotlin/io/github/aretche/controllers/HomeController.kt
+++ src/main/kotlin/io/github/aretche/controllers/HomeController.kt
@@ -5,15 +5,41 @@ import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Produces
 import io.micronaut.security.annotation.Secured
+import io.micronaut.security.token.jwt.generator.JwtTokenGenerator
+import org.slf4j.LoggerFactory
 import java.security.Principal
+import javax.inject.Inject
 
-@Secured("isAuthenticated()")
 @Controller("/")
-class HomeController {
+class HomeController(@Inject val jwtTokenGenerator: JwtTokenGenerator) {
 
+    @Secured("isAuthenticated()")
     @Produces(MediaType.TEXT_PLAIN)
     @Get("/")
     fun index(principal: Principal): String {
+        LOG.info("Ingresando a /")
         return principal.name
+    }
+
+    @Secured("isAnonymous()")
+    @Produces(MediaType.TEXT_PLAIN)
+    @Get("/custom")
+    fun customJWT(): String {
+        LOG.info("Ingresando a /custom")
+        val customClaims = mapOf<String, Any>(
+                "name" to "Jack",
+                "email" to "jack@example.com",
+                "twitter" to "@Jack"
+        )
+        val token = jwtTokenGenerator.generateToken(customClaims)
+        return if (token.isPresent) {
+            token.get()
+        } else {
+            "Token not generated"
+        }
+    }
+
+    companion object {
+        private val LOG = LoggerFactory.getLogger(HomeController::class.java)
     }
 }
